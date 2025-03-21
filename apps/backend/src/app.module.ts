5a52d82ed92fe17ebe17e0d3d9119efb66225bca
SeguraStward <stwardsegura239@gmail.com>
2025-03-20 19:27:15 -0600

    init:login branch

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,17 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { LoggerMiddleware } from '@core/http/middlewares/logger.middleware';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    JwtModule.register({}),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   providers: [PrismaService],
   controllers: [AppController],
 })
