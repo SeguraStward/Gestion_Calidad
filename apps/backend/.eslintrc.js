904a45e2abc723aff4e4656f896476eea481bd7c
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2025-04-18 23:32:50 -0600

    feat: Add VSCode settings and Jest configuration for improved development experience fix: Update TypeScript configuration for better path aliasing and output directory chore: Clean up .gitignore and project words list

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -18,6 +18,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error', { endOfLine: 'auto' }], // Fix, for the moment, for Windows line endings, is recomende use LF instead of CRLF
   },
 };
