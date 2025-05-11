d3ad93f252099722acd1f7923727ce90755acb5d
Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
2025-05-10 18:44:39 -0600

    feat: create Page component to render BulkImportPage for academic management

---
+++ apps/frontend/src/app/academic-management/bulk-import/page.tsx
@@ -0,0 +1,5 @@
+import { BulkImportPage } from '../../../modules/academic-management/bulk-data-import/components/bulk-import-page'
+
+export default function Page() {
+  return <BulkImportPage />
+}
