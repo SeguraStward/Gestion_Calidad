2e22963f63104331983a63b192f0ae4527e4d451
Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
2025-05-24 11:22:39 -0600

    feat: refactor FormField component to use useController for improved form handling

--- apps/frontend/src/app/(components)/form/field.tsx
+++ apps/frontend/src/app/(components)/form/field.tsx
@@ -1,14 +1,25 @@
+'use client'
+
+import { useController, Control } from 'react-hook-form'
 import { Input } from '@una-gc/ui/components/input'
 import { Label } from '@una-gc/ui/components/label'
 import { FieldError, UseFormRegisterReturn } from 'react-hook-form'
 
+// Definir el tipo FormData
+type FormData = {
+  startTime: string
+  endTime: string
+}
+
+// Componente FormField
 interface FormFieldProps {
   label: string
   id: string
-  type?: 'text' | 'number' | 'email' | 'password' | 'date'
+  type?: 'text' | 'number' | 'email' | 'password' | 'date' | 'tel' | 'url' | 'time'
   placeholder?: string
   error?: FieldError
-  register: UseFormRegisterReturn
+  control: Control<FormData> // Asegúrate de que Control es de tipo FormData
+  name: keyof FormData // El nombre del campo debe coincidir con las claves de FormData
   required?: boolean
   disabled?: boolean
 }
@@ -19,23 +30,38 @@ export const FormField = ({
   type = 'text',
   placeholder = '',
   error,
-  register,
+  control,
+  name,
   required = false,
   disabled = false
-}: FormFieldProps) => (
-  <div className="space-y-1">
-    <Label htmlFor={id}>
-      {label}
-      {required && <span className="text-red-500">*</span>}
-    </Label>
-    <Input
-      id={id}
-      type={type}
-      placeholder={placeholder}
-      {...register}
-      disabled={disabled}
-      className={error ? 'border-red-500' : ''}
-    />
-    {error && <p className="text-sm text-red-500">{error.message}</p>}
-  </div>
-)
+}: FormFieldProps) => {
+  const {
+    field: { onChange, value }
+  } = useController<FormData>({
+    // Asegúrate de que useController tenga el tipo FormData
+    control,
+    name,
+    defaultValue: '' // Ajusta el valor por defecto si es necesario
+  })
+
+  return (
+    <div className="space-y-1">
+      <Label htmlFor={id}>
+        {label}
+        {required && <span className="text-red-500">*</span>}
+      </Label>
+      <Input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => {
+          onChange(e) // Esto asegura que 'react-hook-form' maneje el cambio correctamente
+        }}
+        disabled={disabled}
+        className={error ? 'border-red-500' : ''}
+      />
+      {error && <p className="text-sm text-red-500">{error.message}</p>}
+    </div>
+  )
+}
