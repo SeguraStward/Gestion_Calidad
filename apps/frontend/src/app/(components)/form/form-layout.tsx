b06627253f175eb6f2ef8a22c90df7b60a37fac7
Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
2025-05-24 11:23:01 -0600

    feat: enhance FormLayout component to support dynamic className and title types

--- apps/frontend/src/app/(components)/form/form-layout.tsx
+++ apps/frontend/src/app/(components)/form/form-layout.tsx
@@ -1,14 +1,17 @@
+'use client'
+
 import { ReactNode } from 'react'
 
 interface FormLayoutProps {
-  title: string
+  title: string | ReactNode
   children: ReactNode
   onSubmit: (e: React.FormEvent) => void
   footer?: ReactNode
+  className?: string // <---- agregamos
 }
 
-export const FormLayout = ({ title, children, onSubmit, footer }: FormLayoutProps) => (
-  <form onSubmit={onSubmit} className="space-y-6 w-full max-w-3xl">
+export const FormLayout = ({ title, children, onSubmit, footer, className = '' }: FormLayoutProps) => (
+  <form onSubmit={onSubmit} className={`space-y-6 w-full max-w-3xl ${className}`}>
     <h2 className="text-2xl font-bold">{title}</h2>
     {children}
     {footer}
