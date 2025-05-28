ca8ea793f6ccadf0c90c7e9aa9a8bcc8afee37a7
Esteban Javier Granados Sibaja <154352986+EstebanJavierGranadosSibaja@users.noreply.github.com>
2025-05-27 21:02:04 -0600

    feat: update Course type properties to English for consistency and clarity

--- apps/frontend/src/modules/types/curricular/course.ts
+++ apps/frontend/src/modules/types/curricular/course.ts
@@ -1,8 +1,10 @@
 export type Course = {
   id: string
-  nombre: string
-  codigo: string
-  creditos: number
-  descripcion?: string
-  programaId: string
+  name: string
+  code: string
+  description?: string
+  credits: number
+  level: string
+  contactHours: number
+  programaId: string // This will need to be updated to schoolId and careerId later
 }
